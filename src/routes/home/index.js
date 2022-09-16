d823024fce18e3b3937a57e3d7cd528627e56e2e
Parker Bjur <38863599+parkerbjur@users.noreply.github.com>
2022-09-16 11:53:53 -0700

    update Home to load Libraries from server

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,13 +1,33 @@
+import axios from 'axios';
+import { useState, useEffect } from 'preact/hooks';
 import { h } from 'preact';
+import { Button } from '@mui/material';
 import style from './style.css';
-import BookList from '../../components/bookList/BookList.jsx';
+import LibraryList from '../../components/libraryList/LibraryList.jsx';
 
-const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-        <BookList />
-	</div>
-);
+const Home = () => {
+    const [libraries, setLibraries] = useState([]);
+    
+    useEffect(() => { 
+        axios.get('http://parlibre-env.eba-3e823xix.us-west-1.elasticbeanstalk.com/library').then((res) => {
+            setLibraries(res.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, []);
+
+    const addLibrary = () => {
+        window.location.assign('/library/newLibrary');     
+    }
+
+    return (
+        <div class={style.home}>
+            <h1>Home</h1>
+            <Button variant='outlined' onClick={addLibrary}> Add Library </Button>
+            <LibraryList libraries={libraries} />
+        </div>
+    )
+	
+};
 
 export default Home;
