5cddda5e1f3dacc512d5634a6765851ce3f6e234
Parker Bjur <38863599+parkerbjur@users.noreply.github.com>
2022-09-16 11:54:52 -0700

    set library to load book info from server

--- src/routes/library/index.js
+++ src/routes/library/index.js
@@ -1,15 +1,34 @@
-import { h } from 'preact';
+import axios from 'axios';
+import { h} from 'preact';
+import { useState, useEffect } from 'preact/hooks';
 import style from './style.css';
+import { Button } from '@mui/material';
+import BookList from '../../components/bookList/BookList.jsx';
 
-// Note: `user` comes from the URL, courtesy of our router
+// Note: `library` comes from the URL, courtesy of our router
 const Library = ({ library }) => {
+
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://parlibre-env.eba-3e823xix.us-west-1.elasticbeanstalk.com/library/${library}`).then((res) => {
+            setBooks(res.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }, [library]);
+
+    const buttonAction = () => {
+        alert("button pressed");
+    }
 
     return (
 		<div class={style.profile}>
-			<h1>Library Name: {library}</h1>
+			<h1>Library Name: { library }</h1>
 			<p>welcome to { library }.</p>
-
-
+            <Button variant='contained' onClick={buttonAction} sx={{ margin: '5px', mb: '15px' }}> Check-Out </Button>
+            <Button variant='outlined' onClick={buttonAction} sx={{ margin: '5px', mb: '15px' }}> Deposit </Button>
+            <BookList books={books} />
 		</div>
 	);
 }
