5d02a1095a7557d526a0a8f253b1e39c0bdf0a4a
Parker Bjur <38863599+parkerbjur@users.noreply.github.com>
2022-09-16 11:52:09 -0700

    remove dummy data from BookList

--- src/components/bookList/BookList.jsx
+++ src/components/bookList/BookList.jsx
@@ -1,21 +1,7 @@
 import BookCard from '../bookCard/BookCard.jsx';
 
-const BookList = () => {
-    const books = [
-        {
-            author:"Brandon Sanderson",
-            title:"Hero of Ages",
-            cover:"../../assets/mistbornCover.jpeg",
-            genre:"fantasy",
-        },
-        {
-            author:"JK Rowling",
-            title:"Harry Potter",
-            cover:"../../assets/hpCover.jpg",
-            genre:"fantasy",
-        },
-    ]
-
+const BookList = (props) => {
+    const { books } = props;
 	return (
         <div>
             {books.map((book) => 
