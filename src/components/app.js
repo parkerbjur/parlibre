d4b42760d2c8abd0afe7541cd332443572f24429
bjur.parker45@gmail.com <bjur.parker45@gmail.com>
2022-12-10 12:24:45 -0800

    add SanitizeHashPrefixFunction

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
-import { h } from 'preact';
-import { Router } from 'preact-router';
+import { Router, route } from 'preact-router';
 
 import Header from './header';
 
@@ -9,15 +8,28 @@ import Profile from '../routes/profile';
 import Library from '../routes/library';
 import LibraryCreator from '../routes/libraryCreator';
 
+/**
+ * Removes the #! injected into the url
+ * A #! is prefixed to all requests sent to our S3 instance so that we send the index no matter what path is requested
+ * This allows the Router component to handle Routing and prevents 404/403 errors from requesting files which don't exist
+ * [More Info](https://via.studio/journal/hosting-a-reactjs-app-with-routing-on-aws-s3)
+ */
+const sanitizeHashPrefix = () => {
+	const path = (/#!(\/.*)$/.exec(location.hash) || [])[1];
+	if (path) {
+		route(path);
+	}
+}
+
 const App = () => (
 	<div id="app">
 		<Header />
-		<Router>
+		<Router onChange={sanitizeHashPrefix()}>
 			<Home path="/" />
 			<Profile path="/profile/" user="me" />
 			<Profile path="/profile/:user" />
-            <Library path="/library/:library" />
-            <LibraryCreator path="/library/newLibrary" />
+			<Library path="/library/:library" />
+			<LibraryCreator path="/library/newLibrary" />
 		</Router>
 	</div>
 )
