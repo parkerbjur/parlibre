1549d0b737283ae99ef6acc69122e2ff5b7c0a1e
bjur.parker45@gmail.com <bjur.parker45@gmail.com>
2022-12-11 14:15:25 -0800

    move sanitizeHashPrefix to its own component

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
-import { Router, route } from 'preact-router';
+import { Router } from './router/Router';
 
 import Header from './header';
 
@@ -8,23 +8,10 @@ import Profile from '../routes/profile';
 import Library from '../routes/library';
 import LibraryCreator from '../routes/libraryCreator';
 
-/**
- * Removes the #! injected into the url
- * A #! is prefixed to all requests sent to our S3 instance so that we send the index no matter what path is requested
- * This allows the Router component to handle Routing and prevents 404/403 errors from requesting files which don't exist
- * [More Info](https://via.studio/journal/hosting-a-reactjs-app-with-routing-on-aws-s3)
- */
-const sanitizeHashPrefix = () => {
-	const path = (/#!(\/.*)$/.exec(location.hash) || [])[1];
-	if (path) {
-		route(path);
-	}
-}
-
 const App = () => (
 	<div id="app">
 		<Header />
-		<Router onChange={sanitizeHashPrefix()}>
+		<Router >
 			<Home path="/" />
 			<Profile path="/profile/" user="me" />
 			<Profile path="/profile/:user" />
---
+++ src/components/router/Router.jsx
@@ -0,0 +1,28 @@
+import {Router, route} from 'preact-router';
+
+class ParlibreRouter extends Router {
+  constructor(props){
+    super(props);
+  }
+
+  /**
+   * Removes the #! injected into the url
+   * A #! is prefixed to all requests sent to our S3 instance so that we send the index no matter what path is requested
+   * This allows the Router component to handle Routing and prevents 404/403 errors from requesting files which don't exist
+   * [More Info](https://via.studio/journal/hosting-a-reactjs-app-with-routing-on-aws-s3)
+   */
+  sanitizeHashPrefix = () => {
+    const path = (/#!(\/.*)$/.exec(location.hash) || [])[1];
+    if (path) {
+      route(path);
+    }
+  }
+
+  render(){
+    return(
+      <Router onChange={this.sanitizeHashPrefix()}>{this.props.children}</Router>
+    )
+  }
+}
+
+export { ParlibreRouter as Router };
