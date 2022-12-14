62df94e4e2184cdd38ee6fc6b519054c71667350
bjur.parker45@gmail.com <bjur.parker45@gmail.com>
2022-12-14 11:52:08 -0800

    add preact.config.js to load environment variables

---
+++ preact.config.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+const environmentVariables = dotenv.config().parsed;
+
+/**
+ * @param {import('preact-cli').Config} config - Original webpack config
+ * @param {import('preact-cli').Env} env - Current environment info
+ * @param {import('preact-cli').Helpers} helpers - Object with useful helpers for working with the webpack config
+ */
+export default (config, env, helpers) => {
+  const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
+
+  for (const key in environmentVariables) {
+    plugin.definitions[`process.env.${key}`] = JSON.stringify(environmentVariables[key]);
+  }
+}
