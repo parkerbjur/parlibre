82c5ab14ca70abac46617a25497b65ed86b207f6
bjur.parker45@gmail.com <bjur.parker45@gmail.com>
2022-12-14 14:17:00 -0800

    load env variables from code build

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-const environmentVariables = dotenv.config().parsed;
 
 /**
  * @param {import('preact-cli').Config} config - Original webpack config
@@ -7,10 +6,19 @@ const environmentVariables = dotenv.config().parsed;
  * @param {import('preact-cli').Helpers} helpers - Object with useful helpers for working with the webpack config
  */
 export default (config, env, helpers) => {
+  const fileEnv = dotenv.config().parsed;
+  const codeBuildEnv = dotenv.config({ path: `.env` }).parsed;
+  const { API_BASE } = codeBuildEnv
+  let mergedEnv = { ...fileEnv, API_BASE };
+
   const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
-  console.log("---DOTENV SETUP---")
-  for (const key in environmentVariables) {
-    console.log(key, environmentVariables[key]);
-    plugin.definitions[`process.env.${key}`] = JSON.stringify(environmentVariables[key]);
+
+  console.log("---ENV SETUP---");
+  console.log("fileEnv", fileEnv);
+  console.log("codeBuildEnv", fileEnv);
+  console.log("mergedEnv", fileEnv);
+  for (const key in mergedEnv) {
+    console.log(key, mergedEnv[key]);
+    plugin.definitions[`process.env.${key}`] = JSON.stringify(mergedEnv[key]);
   }
 }
